1bdf2dba1bfab00d421c1b96d9d98f662d0a1c6e
Haohang-Linux <hhhuang313@gmail.com>
2021-08-27 19:07:20 -0500

    original version

--- Segmentation3D/PointGroup/lib/spconv/src/spconv/all.cc
+++ Segmentation3D/PointGroup/lib/spconv/src/spconv/all.cc
@@ -17,7 +17,7 @@
 #include <spconv/spconv_ops.h>
 
 static auto registry =
-    torch::RegisterOperators("spconv::get_indice_pairs_2d", &spconv::getIndicePair<2>)
+    torch::jit::RegisterOperators("spconv::get_indice_pairs_2d", &spconv::getIndicePair<2>)
         .op("spconv::get_indice_pairs_3d", &spconv::getIndicePair<3>)
         .op("spconv::get_indice_pairs_grid_2d", &spconv::getIndicePairPreGrid<2>)
         .op("spconv::get_indice_pairs_grid_3d", &spconv::getIndicePairPreGrid<3>)
